6f4a220c4390f1752d73a1c0c742c5e07f9da890
Simon Racine <simon.racine@hotmail.com>
2016-07-24 15:49:46 -0400

    Start_movie.cs corrigé pour qu'il n'empêche plus de builder sur le téléphone. En utilisant #if UNITY_EDITOR && UNITY_ANDROID

--- Assets/Script/Start_movie.cs
+++ Assets/Script/Start_movie.cs
@@ -2,21 +2,17 @@
 using System.IO;
 
 public class Start_movie : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         // Start the movie
+        #if UNITY_EDITOR && UNITY_ANDROID
         string fullPath = Path.Combine(Path.Combine("Assets", "StreamingAssets"), Path.Combine("Movies", "videoTemp1.mp4"));
-        //Handheld.PlayFullScreenMovie(fullPath);
 
             ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
-
-
+        #endif
+        
     }
 
-    // Update is called once per frame
-    void Update() {
 
-          
-    }
-
+   
 }
