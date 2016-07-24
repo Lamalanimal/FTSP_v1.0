fe4f3f4e4aa83972c0f3fa57793d978c9aab4269
Simon Racine <simon.racine@hotmail.com>
2016-07-24 14:43:19 -0400

    Ajout d'une scène3 qui permet de tester en unity. Ajout du script MousseCamera.cs

--- Assets/Script/Start_movie.cs
+++ Assets/Script/Start_movie.cs
@@ -5,14 +5,10 @@ public class Start_movie : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // Start the movie
-
-#if UNITY_ANDROID
         string fullPath = Path.Combine(Path.Combine("Assets", "StreamingAssets"), Path.Combine("Movies", "videoTemp1.mp4"));
-        Handheld.PlayFullScreenMovie(fullPath);
-#else
-            ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
-#endif
+        //Handheld.PlayFullScreenMovie(fullPath);
 
+            ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
 
 
     }
